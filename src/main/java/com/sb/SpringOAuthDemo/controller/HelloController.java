423b74e39c2bc2eff45f4a7cc43c8b28d0087d17
Bharti <sahanibharti71@gmail.com>
2026-03-29 23:58:18 +0530

    Hellocontroller added with OAuth dependency disabled to see the default behaviour

---
+++ src/main/java/com/sb/SpringOAuthDemo/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.sb.SpringOAuthDemo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String greet(){
+        return "Hello, welcome to Spring Security with OAuth2!";
+    }
+}
